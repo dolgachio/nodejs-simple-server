336b8a79bbf8bbf06f6449a5694424e828643fb1
Stanislav Dolgachov <s.dolgachov@gmail.com>
2020-10-25 22:49:36 +0200

    feat: add init DB script

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const { PORT } = require('./common/config');
 const app = require('./app');
+const initDB = require('./utils/db/init.db');
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+initDB().then(() => {
+  app.listen(PORT, () =>
+    console.log(`App is running on http://localhost:${PORT}`)
+  );
+});
